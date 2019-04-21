19e977db3729f16b10aa54282abe8471f4b46953
Kevin Risqi <kevinrisqi22@gmail.com>
2019-04-21 23:08:21 +0700

    Pembuatan form penjualan

---
+++ FormPenjualan.vb
@@ -0,0 +1,14 @@
+﻿Public Class FormPenjualan
+
+    Private Sub FormPenjualan_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        If e.KeyChar = Chr(Keys.Escape) Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub FormPenjualan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+End Class
--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.AplikasiPenjualan.FormMenuUtama
+            Me.MainForm = Global.AplikasiPenjualan.FormPenjualan
         End Sub
     End Class
 End Namespace
