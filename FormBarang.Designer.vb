aec8d33ae43ace4a04d0062fc9da3a4319cba498
Kevin Risqi <kevinrisqi22@gmail.com>
2019-04-28 14:01:52 +0700

    Penambahan search data

--- FormBarang.Designer.vb
+++ FormBarang.Designer.vb
@@ -22,265 +22,56 @@ Partial Class FormBarang
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox9 = New System.Windows.Forms.TextBox()
-        Me.TextBox8 = New System.Windows.Forms.TextBox()
-        Me.Label10 = New System.Windows.Forms.Label()
-        Me.TextBox7 = New System.Windows.Forms.TextBox()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
-        Me.TextBox6 = New System.Windows.Forms.TextBox()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormBarang))
         Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.idBarang = New System.Windows.Forms.TextBox()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
         Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Button6 = New System.Windows.Forms.Button()
+        Me.search = New Bunifu.Framework.UI.BunifuTextbox()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'TextBox9
-        '
-        Me.TextBox9.Enabled = False
-        Me.TextBox9.Location = New System.Drawing.Point(614, 293)
-        Me.TextBox9.Name = "TextBox9"
-        Me.TextBox9.Size = New System.Drawing.Size(34, 20)
-        Me.TextBox9.TabIndex = 19
-        Me.TextBox9.Visible = False
-        '
-        'TextBox8
-        '
-        Me.TextBox8.Location = New System.Drawing.Point(761, 293)
-        Me.TextBox8.MaxLength = 15
-        Me.TextBox8.Name = "TextBox8"
-        Me.TextBox8.Size = New System.Drawing.Size(82, 20)
-        Me.TextBox8.TabIndex = 5
-        '
-        'Label10
-        '
-        Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(672, 296)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(41, 13)
-        Me.Label10.TabIndex = 17
-        Me.Label10.Text = "Satuan"
-        '
-        'TextBox7
-        '
-        Me.TextBox7.Enabled = False
-        Me.TextBox7.Location = New System.Drawing.Point(849, 293)
-        Me.TextBox7.Name = "TextBox7"
-        Me.TextBox7.Size = New System.Drawing.Size(34, 20)
-        Me.TextBox7.TabIndex = 16
-        Me.TextBox7.Visible = False
-        '
-        'ComboBox1
-        '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(737, 240)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(171, 21)
-        Me.ComboBox1.TabIndex = 2
-        '
-        'RichTextBox1
-        '
-        Me.RichTextBox1.Location = New System.Drawing.Point(539, 345)
-        Me.RichTextBox1.MaxLength = 255
-        Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(369, 52)
-        Me.RichTextBox1.TabIndex = 8
-        Me.RichTextBox1.Text = ""
-        '
-        'TextBox6
-        '
-        Me.TextBox6.Location = New System.Drawing.Point(761, 319)
-        Me.TextBox6.MaxLength = 10
-        Me.TextBox6.Name = "TextBox6"
-        Me.TextBox6.Size = New System.Drawing.Size(122, 20)
-        Me.TextBox6.TabIndex = 7
-        '
-        'TextBox5
-        '
-        Me.TextBox5.Location = New System.Drawing.Point(539, 319)
-        Me.TextBox5.MaxLength = 10
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(122, 20)
-        Me.TextBox5.TabIndex = 6
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Location = New System.Drawing.Point(539, 293)
-        Me.TextBox3.MaxLength = 10
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(69, 20)
-        Me.TextBox3.TabIndex = 4
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(539, 267)
-        Me.TextBox2.MaxLength = 45
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(369, 20)
-        Me.TextBox2.TabIndex = 3
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(539, 240)
-        Me.TextBox1.MaxLength = 6
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(97, 20)
-        Me.TextBox1.TabIndex = 1
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(450, 348)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(62, 13)
-        Me.Label7.TabIndex = 6
-        Me.Label7.Text = "Keterangan"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(672, 319)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(58, 13)
-        Me.Label6.TabIndex = 5
-        Me.Label6.Text = "Harga Jual"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(451, 319)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(56, 13)
-        Me.Label5.TabIndex = 4
-        Me.Label5.Text = "Harga Beli"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(450, 267)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(72, 13)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Nama Barang"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(450, 296)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(29, 13)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Stok"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(649, 243)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(46, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Kategori"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(450, 243)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(69, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Kode Barang"
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(113, 200)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(85, 36)
-        Me.Button2.TabIndex = 17
-        Me.Button2.Text = "Ubah"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(22, 200)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(85, 36)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Tambah"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(204, 200)
+        Me.Button3.Location = New System.Drawing.Point(871, 538)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(85, 36)
         Me.Button3.TabIndex = 18
         Me.Button3.Text = "Hapus"
         Me.Button3.UseVisualStyleBackColor = True
         '
-        'Button4
-        '
-        Me.Button4.Location = New System.Drawing.Point(418, 200)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(85, 36)
-        Me.Button4.TabIndex = 19
-        Me.Button4.Text = "Keluar"
-        Me.Button4.UseVisualStyleBackColor = True
-        '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 403)
+        Me.DataGridView1.Location = New System.Drawing.Point(51, 223)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(976, 184)
+        Me.DataGridView1.Size = New System.Drawing.Size(905, 309)
         Me.DataGridView1.TabIndex = 22
         '
-        'Button5
+        'idBarang
         '
-        Me.Button5.Location = New System.Drawing.Point(295, 200)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(85, 36)
-        Me.Button5.TabIndex = 23
-        Me.Button5.Text = "Batal"
-        Me.Button5.UseVisualStyleBackColor = True
-        '
-        'TextBox4
-        '
-        Me.TextBox4.Location = New System.Drawing.Point(138, 252)
-        Me.TextBox4.MaxLength = 15
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(365, 20)
-        Me.TextBox4.TabIndex = 16
+        Me.idBarang.Enabled = False
+        Me.idBarang.Location = New System.Drawing.Point(523, 192)
+        Me.idBarang.MaxLength = 15
+        Me.idBarang.Name = "idBarang"
+        Me.idBarang.Size = New System.Drawing.Size(29, 20)
+        Me.idBarang.TabIndex = 16
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(31, 255)
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(62, 192)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(86, 13)
+        Me.Label8.Size = New System.Drawing.Size(125, 18)
         Me.Label8.TabIndex = 16
-        Me.Label8.Text = "Pencarian Nama"
+        Me.Label8.Text = "Pencarian Barang"
         '
         'Label9
         '
@@ -323,12 +114,25 @@ Partial Class FormBarang
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(521, 155)
+        Me.Button6.Location = New System.Drawing.Point(841, 170)
         Me.Button6.Name = "Button6"
         Me.Button6.Size = New System.Drawing.Size(115, 46)
         Me.Button6.TabIndex = 27
         Me.Button6.Text = "Button6"
         Me.Button6.UseVisualStyleBackColor = True
+        '
+        'search
+        '
+        Me.search.BackColor = System.Drawing.Color.White
+        Me.search.BackgroundImage = CType(resources.GetObject("search.BackgroundImage"), System.Drawing.Image)
+        Me.search.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.search.ForeColor = System.Drawing.Color.Black
+        Me.search.Icon = CType(resources.GetObject("search.Icon"), System.Drawing.Image)
+        Me.search.Location = New System.Drawing.Point(211, 185)
+        Me.search.Name = "search"
+        Me.search.Size = New System.Drawing.Size(250, 32)
+        Me.search.TabIndex = 29
+        Me.search.text = ""
         '
         'FormBarang
         '
@@ -337,35 +141,14 @@ Partial Class FormBarang
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(1018, 599)
         Me.ControlBox = False
+        Me.Controls.Add(Me.search)
         Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label9)
-        Me.Controls.Add(Me.TextBox9)
         Me.Controls.Add(Me.Label8)
-        Me.Controls.Add(Me.TextBox8)
-        Me.Controls.Add(Me.TextBox4)
-        Me.Controls.Add(Me.Label10)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.idBarang)
         Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.RichTextBox1)
-        Me.Controls.Add(Me.TextBox6)
         Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.TextBox5)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.ShapeContainer1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "FormBarang"
@@ -377,35 +160,14 @@ Partial Class FormBarang
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents Label7 As System.Windows.Forms.Label
-    Friend WithEvents Label6 As System.Windows.Forms.Label
-    Friend WithEvents Label5 As System.Windows.Forms.Label
-    Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
-    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Button3 As System.Windows.Forms.Button
-    Friend WithEvents Button4 As System.Windows.Forms.Button
     Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
-    Friend WithEvents Button5 As System.Windows.Forms.Button
-    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents idBarang As System.Windows.Forms.TextBox
     Friend WithEvents Label8 As System.Windows.Forms.Label
-    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
-    Friend WithEvents Label10 As System.Windows.Forms.Label
-    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
     Friend WithEvents Label9 As System.Windows.Forms.Label
     Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
     Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents search As Bunifu.Framework.UI.BunifuTextbox
 End Class
--- FormBarang.vb
+++ FormBarang.vb
@@ -2,13 +2,10 @@
 Public Class FormBarang
 
     Private Sub FormBarang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Call tampilBarang()
-        Call kondisiAwal()
-        Call dgv()
-        Call combobox()
+        Call tampilBarang()
     End Sub
 
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
         Me.Close()
     End Sub
 
@@ -23,55 +20,9 @@ Public Class FormBarang
     End Sub
 
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If Button1.Text = "Tambah" Then
-            TextBox1.Focus()
-            Button1.Text = "Simpan"
-            Call isiData()
-        Else
-            If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox5.Text = "" Or TextBox6.Text = "" Or TextBox7.Text = "" Or TextBox8.Text = "" Or RichTextBox1.Text = "" Or ComboBox1.Text = "" Then
-                MsgBox("Silahkan isi data dengan lengkap !", vbInformation)
-            Else
-                Call koneksi()
-                Dim inputData As String = "INSERT INTO barang (id_barang,nama_barang,harga_beli,harga_jual,stok,satuan,keterangan,id_kategori) values ('" & TextBox1.Text & "', '" & TextBox2.Text & "', '" & TextBox5.Text & "', '" & TextBox6.Text & "', '" & TextBox3.Text & "', '" & TextBox8.Text & "', '" & RichTextBox1.Text & "', '" & TextBox7.Text & "')"
-                Cmd = New OdbcCommand(inputData, Conn)
-                Cmd.ExecuteNonQuery()
-                MsgBox("Data berhasil diinputkan")
-                Call kondisiAwal()
-            End If
-        End If
+    
 
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox5.Text = "" Or TextBox6.Text = "" Or TextBox7.Text = "" Or TextBox8.Text = "" Or RichTextBox1.Text = "" Or ComboBox1.Text = "" Then
-            MsgBox("Silahkan isi data dengan lengkap !", vbInformation)
-        Else
-            Call koneksi()
-            Dim updateData As String = "UPDATE barang SET id_barang = '" & TextBox1.Text & "',nama_barang = '" & TextBox2.Text & "', harga_beli = '" & TextBox5.Text & "', harga_jual = '" & TextBox6.Text & "',stok = '" & TextBox3.Text & "', satuan = '" & TextBox8.Text & "', keterangan = '" & RichTextBox1.Text & "',id_kategori = '" & TextBox7.Text & "' WHERE id = '" & TextBox9.Text & "' "
-            Cmd = New OdbcCommand(updateData, Conn)
-            Cmd.ExecuteNonQuery()
-            MsgBox("Data berhasil diubah")
-            Call kondisiAwal()
-        End If
-    End Sub
-
-    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
-        Call updateData()
-        Dim i As Integer
-        i = DataGridView1.CurrentRow.Index
-
-        TextBox9.Text = DataGridView1.Item(0, i).Value
-        TextBox1.Text = DataGridView1.Item(1, i).Value
-        TextBox2.Text = DataGridView1.Item(2, i).Value
-        TextBox5.Text = DataGridView1.Item(3, i).Value
-        TextBox6.Text = DataGridView1.Item(4, i).Value
-        TextBox3.Text = DataGridView1.Item(5, i).Value
-        TextBox8.Text = DataGridView1.Item(6, i).Value
-        RichTextBox1.Text = DataGridView1.Item(7, i).Value
-        TextBox7.Text = DataGridView1.Item(8, i).Value
-
-    End Sub
+ 
 
     
 
@@ -100,149 +51,14 @@ Public Class FormBarang
         DataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.Silver
     End Sub
 
-
-    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
-        Call showCategoriById()
-    End Sub
-
-    Sub combobox()
-        Call koneksi()
-        Dim combo As String = "SELECT DISTINCT kategori_barang.nama_kategori FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori"
-        Cmd = New OdbcCommand(combo, Conn)
-        Rd = Cmd.ExecuteReader
-        Do While Rd.Read = True
-            ComboBox1.Items.Add(Rd.Item("nama_kategori"))
-        Loop
-    End Sub
-
-    Sub showCategoriById()
-        Call koneksi()
-        Dim showcat As String = "SELECT * FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori WHERE kategori_barang.nama_kategori = '" & ComboBox1.Text & "'"
-        Cmd = New OdbcCommand(showcat, Conn)
-        Rd = Cmd.ExecuteReader
-        Rd.Read()
-        If Rd.HasRows Then
-            TextBox7.Text = Rd.Item("id_kategori")
-        End If
-    End Sub
-
-    Sub kondisiAwal()
-        TextBox1.Text = ""
-        TextBox2.Text = ""
-        TextBox3.Text = ""
-        TextBox5.Text = ""
-        TextBox6.Text = ""
-        TextBox8.Text = ""
-        RichTextBox1.Text = ""
-        'ComboBox1.Text = ""
-
-        TextBox1.Enabled = False
-        TextBox2.Enabled = False
-        TextBox3.Enabled = False
-        TextBox5.Enabled = False
-        TextBox6.Enabled = False
-        TextBox8.Enabled = False
-        RichTextBox1.Enabled = False
-        ComboBox1.Enabled = False
-
-        Button1.Text = "Tambah"
-        Button1.Enabled = True
-        Button2.Enabled = False
-        Button3.Enabled = False
-        Button5.Enabled = False
-        DataGridView1.Enabled = True
-
-        Call tampilBarang()
-    End Sub
-
-    Sub isiData()
-        TextBox1.Focus()
-        TextBox1.Enabled = True
-        TextBox2.Enabled = True
-        TextBox3.Enabled = True
-        TextBox5.Enabled = True
-        TextBox6.Enabled = True
-        TextBox8.Enabled = True
-        RichTextBox1.Enabled = True
-        ComboBox1.Enabled = True
-        DataGridView1.Enabled = False
-        Button5.Enabled = True
-    End Sub
-
-    Sub updateData()
-        TextBox1.Focus()
-        TextBox1.Enabled = True
-        TextBox2.Enabled = True
-        TextBox3.Enabled = True
-        TextBox5.Enabled = True
-        TextBox6.Enabled = True
-        TextBox8.Enabled = True
-        RichTextBox1.Enabled = True
-        ComboBox1.Enabled = True
-        DataGridView1.Enabled = True
-        Button5.Enabled = True
-        Button2.Enabled = True
-        Button3.Enabled = True
-        Button1.Enabled = False
-    End Sub
-    
-    Private Sub TextBox7_TextChanged(sender As Object, e As EventArgs) Handles TextBox7.TextChanged
-
-    End Sub
-
-    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-        Call kondisiAwal()
-    End Sub
-
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         If MsgBox("Apakah Anda ingin menghapusnya ?", vbInformation + vbYesNo) = vbYes Then
             Call koneksi()
-            Dim deleteData As String = "DELETE FROM barang WHERE id = '" & TextBox9.Text & "'"
+            Dim deleteData As String = "DELETE FROM barang WHERE id = '" & idBarang.Text & "'"
             Cmd = New OdbcCommand(deleteData, Conn)
             Cmd.ExecuteNonQuery()
             MsgBox("Data berhasil dihapus")
-            Call kondisiAwal()
         End If
-    End Sub
-
-    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
-        Call koneksi()
-        Dim searchData As String = "SELECT * FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori WHERE barang.nama_barang like '%" & TextBox4.Text & "%'"
-        Cmd = New OdbcCommand(searchData, Conn)
-        Rd = Cmd.ExecuteReader
-        Rd.Read()
-        If Rd.HasRows Then
-            Call koneksi()
-            Dim query As String = "SELECT * FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori WHERE barang.nama_barang like '%" & TextBox4.Text & "%'"
-            Da = New OdbcDataAdapter(query, Conn)
-            Ds = New DataSet
-            Da.Fill(Ds, "barang")
-            DataGridView1.DataSource = Ds.Tables("barang")
-        End If
-    End Sub
-
-    Private Sub TextBox3_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox3.KeyPress
-        If Not ((e.KeyChar >= "0" And e.KeyChar <= "9") Or e.KeyChar = vbBack) Then e.Handled = True
-    End Sub
-
-    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
-
-    End Sub
-
-    Private Sub TextBox5_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox5.KeyPress
-        If Not ((e.KeyChar >= "0" And e.KeyChar <= "9") Or e.KeyChar = vbBack) Then e.Handled = True
-    End Sub
-
-    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
-
-    End Sub
-
-    Private Sub TextBox6_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox6.KeyPress
-        If Not ((e.KeyChar >= "0" And e.KeyChar <= "9") Or e.KeyChar = vbBack) Then e.Handled = True
-    End Sub
-
-    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
-
     End Sub
 
     Sub switchPanel(ByVal panel As Form)
@@ -270,5 +86,21 @@ Public Class FormBarang
         FormAddBarang.keterangan.Text = DataGridView1.Item(7, i).Value
         FormAddBarang.kategoriID.Text = DataGridView1.Item(8, i).Value
         switchPanel(FormAddBarang)
+    End Sub
+
+    Private Sub search_OnTextChange(sender As Object, e As EventArgs) Handles search.OnTextChange
+        Call koneksi()
+        Dim searchData As String = "SELECT * FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori WHERE barang.nama_barang like '%" & search.text & "%'"
+        Cmd = New OdbcCommand(searchData, Conn)
+        Rd = Cmd.ExecuteReader
+        Rd.Read()
+        If Rd.HasRows Then
+            Call koneksi()
+            Dim query As String = "SELECT * FROM barang JOIN kategori_barang ON barang.id_kategori = kategori_barang.id_kategori WHERE barang.nama_barang like '%" & search.text & "%'"
+            Da = New OdbcDataAdapter(query, Conn)
+            Ds = New DataSet
+            Da.Fill(Ds, "barang")
+            DataGridView1.DataSource = Ds.Tables("barang")
+        End If
     End Sub
 End Class
